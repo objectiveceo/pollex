8c3086856aea4cf7fb93e46083124bc306232418
Grayson <grayson.hansard@gmail.com>
2021-10-27 18:36:41 -0400

    Add ability to create captions on images

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ interface ElementCreator {
 	body: HTMLElement,
 	createImg: () => HTMLImageElement,
 	createDiv: () => HTMLDivElement,
+	createParagraph: () => HTMLParagraphElement,
 }
 
 function closeImage(background: HTMLDivElement) {
@@ -19,30 +20,63 @@ function imageReady(image: HTMLImageElement, background: HTMLDivElement) {
 	} else {
 		image.style.height = '90%';
 	}
+
+	if (background.lastChild) {
+		image.style.paddingBottom = `${(background.lastChild as HTMLElement).offsetHeight + 16}px`;
+	}
+
 	background.addEventListener('click', () => closeImage(background));
 	background.insertBefore(image, background.lastChild);
 }
 
-function expand(element: HTMLElement, creator: ElementCreator) {
-	const dest = element.dataset.pollexDest;
-	const image = creator.createImg();
+function createBackground(creator: ElementCreator): HTMLDivElement {
 	const background = creator.createDiv();
-	image.onload = (e) => imageReady(image, background);
-	image.src = dest;
-
 	background.style.backgroundColor = 'rgba(0,0,0,0.5)';
 	background.style.position = 'fixed';
 	background.style.top = '0';
 	background.style.right = '0';
 	background.style.bottom = '0';
 	background.style.left = '0';
+	return background;
+}
+
+function createCaption(element: HTMLElement, creator: ElementCreator): HTMLParagraphElement | null {
+	const caption = element.dataset.pollexCaption;
+	if (!caption) {
+		return null;
+	}
+
+	const para = creator.createParagraph();
+	para.innerHTML = caption;
+	para.style.position = 'absolute';
+	para.style.bottom = '0';
+	para.style.width = '100%';
+	para.style.textAlign = 'center';
+	para.style.color = 'white';
+	para.style.textShadow = '#000 0 2px 2px';
+	return para;
+}
+
+function expand(element: HTMLElement, creator: ElementCreator) {
+	const dest = element.dataset.pollexDest;
+	const image = creator.createImg();
+	const background = createBackground(creator);
+	image.onload = (e) => imageReady(image, background);
+	image.src = dest;
+
+	const caption = createCaption(element, creator);
+	if (caption) {
+		background.insertBefore(caption, background.lastChild);
+	}
+
 	creator.body.insertBefore(background, creator.body.lastChild);
 }
 
 function setupThumbnails(element: Element, creator: ElementCreator) {
 	const htmlElement = element as HTMLElement;
-	if (!htmlElement)
+	if (!htmlElement) {
 		return;
+	}
 	htmlElement.addEventListener('click', () => expand(htmlElement, creator));
 	htmlElement.style.cursor = 'pointer';
 }
@@ -53,6 +87,7 @@ function main() {
 		body: document.body,
 		createImg: () => document.createElement('img'),
 		createDiv: () => document.createElement('div'),
+		createParagraph: () => document.createElement('p'),
 	}
 	thumbs.forEach((x) => setupThumbnails(x, creator));
 }
